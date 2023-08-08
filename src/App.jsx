771e464c3ed69cb98ec8b598615988c02fe1b96f
henrix49 <henrix494@gmail.com>
2023-08-08 21:45:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
+import Serv from "./components/serv/Serv";
+import About from "./components/about/About";
+function App() {
+	return (
+		<>
+			{" "}
+			<Navbar />
+			<Hero />
+			<Serv />
+			<About />
+		</>
+	);
+}
+
+export default App;
